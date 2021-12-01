842184bbf32cba05fa1f2af7f8a1555b98f88e8a
Shajjadur-Rahman <shaturngbd@gmail.com>
2021-12-02 02:05:48 +0600

    mmmm

--- JobManagementSystem/settings.py
+++ JobManagementSystem/settings.py
@@ -23,9 +23,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-if DEBUG:
-    ALLOWED_HOSTS += env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['shajjadpms.herokuapp.com', '127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['*']
 
 
 
